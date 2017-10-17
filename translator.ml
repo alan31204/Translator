998de41426d208724feafb88ec5f6cd72ea65248
Alan <pchiu4@u.rochester.edu>
2017-10-16 20:52:51 -0400

    Fixed some redundancies

--- translator.ml
+++ translator.ml
@@ -687,9 +687,77 @@ ast_ize_P tree2;; *)
 (*  commented out so this code will complile *)
 
     (* warnings  output_program *) 
+
+let  helper = "
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <string.h>
+
+    int array[100];
+    char* string[100];
+
+    int getint() {
+      char str[100];
+      int n = scanf(\"%s\", str);
+      if(n != 1){
+        printf(\"Unexpected end of input!\\n\");
+        exit(1);
+      }else{
+        for(int k = 0; str[k]!='\\0' ; k++){
+          if(str[k] >'9' || str[k] <'0'){
+            printf(\"non numeric input!\\n\");
+            exit(1);
+          }
+        }
+        return atoi(str);
+      }
+    }
+
+    void putint(int n) {
+      printf(\"%d\\n\", n);
+    }
+    
+    int divide (int a, int b){
+      if(b == 0){
+        printf(\"A division by zero erro here!\\n\");
+        exit(1);
+      }
+      return a/b ;
+    }
     
 
-let rec translate (ast:ast_sl) (helper:string) :  string *  string = ("", helper^snd(translate_sl (ast))^"}\n")
+    int getvar(char* a){
+      for(int i = 0; string [i] != 0;i++){
+        if(!strcmp(string[i],a)){
+          return array[i];
+        }
+      }
+      printf(\"Use of uninitialized variable %s\\n\", a);
+      exit (1);
+    }
+
+    int setvar(char* a, int n){
+      int i;
+      for(i = 0; string [i] != 0;i++){
+        if(!strcmp(string[i],a)){
+          array[i] = n;
+          return 1;
+        }
+      }
+      string[i] = a;
+      array[i] = n; 
+      return 0;
+    }
+    
+    
+    
+    int main(int argc, char* argv[]) {
+
+
+    ";;
+    
+
+let rec translate (ast:ast_sl) :  string *  string = ("", helper^snd(translate_sl (ast))^"}\n")
 
 and translate_sl (ast:ast_sl)  :  string *  string = 
   match ast with 
@@ -747,79 +815,6 @@ and translate_expr (ast:ast_e)  :  string *  string =
 #use "translator.ml";;
 *)
 (*print_string (snd translate( ast_ize_P tree)) ;;*)
-let  helper = "
-    #include <stdio.h>
-    #include <stdlib.h>
-    #include <string.h>
-
-    int array[100];
-    char* string[100];
-    
-
-    int getint() {
-      char str[100];
-      int n = scanf(\"%s\", str);
-      if(n != 1){
-        printf(\"Unexpected end of input!\\n\");
-        exit(1);
-      }else{
-        for(int k = 0; str[k]!='\\0' ; k++){
-          if(str[k] >'9' || str[k] <'0'){
-            printf(\"non numeric input!\\n\");
-            exit(1);
-          }
-        }
-        return atoi(str);
-      }
-    }
-
-    void putint(int n) {
-      printf(\"%d\\n\", n);
-    }
-
-    int divisionE(){
-      printf(\"A division by zero erro here!\\n\");
-      exit(1);
-      return 1;
-    }
-    
-    int divide (int a, int b){
-      if(b == 0){
-        divisionE();
-      }
-      return a/b ;
-    }
-    
-
-    int getvar(char* a){
-      for(int i = 0; string [i] != 0;i++){
-        if(!strcmp(string[i],a)){
-          return array[i];
-        }
-      }
-      printf(\"Use of uninitialized variable %s\\n\", a);
-      exit (1);
-    }
-
-    int setvar(char* a, int n){
-      int i;
-      for(i = 0; string [i] != 0;i++){
-        if(!strcmp(string[i],a)){
-          array[i] = n;
-          return 1;
-        }
-      }
-      string[i] = a;
-      array[i] = n; 
-      return 0;
-    }
-    
-    
-    
-    int main(int argc, char* argv[]) {
-
-
-    ";;
 
 
 let prog = "read a
@@ -827,8 +822,8 @@ let prog = "read a
         write a / b";;    
 
 
-(* print_string (snd (translate (ast_ize_P (parse ecg_parse_table primes_prog)) helper));; *)
+print_string (snd (translate (ast_ize_P (parse ecg_parse_table primes_prog))));;
 
 
-print_string (snd (translate (ast_ize_P (parse ecg_parse_table prog)) helper));; 
+(*print_string (snd (translate (ast_ize_P (parse ecg_parse_table prog)) helper));; *) 
 
